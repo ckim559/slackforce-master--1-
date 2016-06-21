195e48353aba17eaa0f655e585631d497af73d21
ckim559 <ckim@freewheel.tv>
2016-06-21 18:11:07 -0400

    Update case.js

--- modules/case.js
+++ modules/case.js
@@ -10,7 +10,7 @@ function execute(req, res) {
         return;
     }
 
-     var q = "SELECT Id, Name, Phone, Account_Owner__c, Type, BillingStreet, BillingCity, BillingState, Account_Manager__c, Solutions_Engineer__c, PGM__c FROM Account WHERE Name LIKE '%" + req.body.text + "%' LIMIT 10";
+     var q = "SELECT Id, Name, Phone, Account_Owner__c, Type, BillingStreet, BillingCity, BillingState, AM_Text__c, SE_Text__c, PGM_Text__c FROM Account WHERE Name LIKE '%" + req.body.text + "%' LIMIT 10";
     org.query({query: q}, function(err, resp) {
         if (err) {
             console.error(err);
@@ -24,9 +24,9 @@ function execute(req, res) {
                 var fields = [];
                 fields.push({title: "Name:", value: account.get("Name"), short:true});
                 fields.push({title: "Owner:", value: account.get("Account_Owner__c"), short:true});
-                fields.push({title: "AM:", value: account.get("Account_Manager__c"), short:true});
-                fields.push({title: "SE:", value: account.get("Solutions_Engineer__c"), short:true});
-                fields.push({title: "PGM:", value: account.get("PGM__c"), short:true});
+                fields.push({title: "Account Manager:", value: account.get("AM_Text__c"), short:true});
+                fields.push({title: "Solutions Engineer:", value: account.get("SE_Text__c"), short:true});
+                fields.push({title: "PGM:", value: account.get("PGM_Text__c"), short:true});
                 fields.push({title: "Account Type:", value: account.get("Type"), short:true});
                 fields.push({title: "Link", value: "https://login.salesforce.com/" + account.getId(), short:true});
                 attachments.push({color: "#009cdb", fields: fields});
